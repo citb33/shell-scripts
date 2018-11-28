9a3b822f946b9e0fb9ec67f40c04770d7db7efde
Raghu K <raghuk.vit@gmail.com>
2018-11-28 02:52:26 +0000

    Updating shell-scripts

--- stack.sh
+++ stack.sh
@@ -84,4 +84,4 @@ Stat $?
 
 Print "Configuring HTTPD"
 PUBLICIP=$(curl ifconfig.me)
-echo $INDEX_HTML | sed -e "s/PUBLICIP/$PUBLICIP/" >/var/www/html/index.
+echo $INDEX_HTML | sed -e "s/PUBLICIP/$PUBLICIP/" >/var/www/html/index.html 
