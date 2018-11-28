abdc2e9abefac5acaa2ac38b4e9e8ddc23dfd4bc
Raghu K <raghuk.vit@gmail.com>
2018-11-28 02:47:36 +0000

    Updating shell-scripts

--- stack.sh
+++ stack.sh
@@ -67,4 +67,5 @@ chown student:student $APPHOME/apache-tomcat-$TOMCAT_VER -R
 Print "Configuring Tomcat"
 sed -i -e '/TestDB/ d' -e "$ i $JDBC_CON_URI" $APPHOME/apache-tomcat-$TOMCAT_VER/conf/context.xml 
 sed -i -e "s/USERNAME/$USERNAME/" -e "s/PASSWORD/$PASSWORD/" -e "s/DBURL/$DBURL/" -e "s/DBNAME/$DBNAME/" $APPHOME/apache-tomcat-$TOMCAT_VER/conf/context.xml
-wget -q https://raw.githubusercontent.com/citb33/project-documentation/master/tomcat-init-script -O /etc
+wget -q https://raw.githubusercontent.com/citb33/project-documentation/master/tomcat-init-script -O /etc/init.d/tomcat 
+
