66117be625eb25a07d641d01c267faa25e383a9d
Raghu K <raghuk.vit@gmail.com>
2018-11-28 02:27:19 +0000

    Updating shell-scripts

--- stack.sh
+++ stack.sh
@@ -8,7 +8,7 @@ APPHOME=/home/$APPUSER
 TOMCAT_VER="9.0.13"
 TOMCAT_MAJ_VER=$(echo $TOMCAT_VER | cut -d . -f1)
 URL="http://mirrors.wuchna.com/apachemirror/tomcat/tomcat-${TOMCAT_MAJ_VER}/v${TOMCAT_VER}/bin/apache-tomcat-${TOMCAT_VER}.tar.gz"
-STUDENT_WAR
+STUDENT_WAR_URL="https://github.com/citb33/project-documentation/raw/master/studentapp.war"
 
 
 USER=$(id -u)
