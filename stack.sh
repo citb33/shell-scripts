8f111ba152589bb3f94bb3b05810c71f16014dba
Raghu K <raghuk.vit@gmail.com>
2018-11-28 02:07:40 +0000

    Updating shell-scripts

---
+++ stack.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## Script purpose is to install both application and web server.
+LOG=/tmp/stack.log 
+rm -f $LOG
+
+USER=$(id -u)
+if [ $USER -ne 0 ]; then 
+    echo -e "\e[31mYou should be a root user to execute this script\e[0m"
+    exit 5
+fi
+
+Print() {
+    
+}
+
+Print "Installing Java"
+yum install java -y &>>$LOG 
+if [ $? -eq 0 ]; then 
+    echo -e "\e[32mSUCCESS\e[0m"
+else
+    echo -e "\e[31mFAILED\e[0m"
+fi
+#
+
+yum ins
